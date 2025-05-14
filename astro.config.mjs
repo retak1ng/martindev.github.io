cf6a6f4df8f70284e8f70639ca1490707f79b75d
Martín Rojas <csamuelr.lp@gmail.com>
2025-05-14 20:11:01 -0300

    deploy fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://retak1ng.github.io',
+	base: 'my-repo',
 	integrations: [mdx(), sitemap()],
 });
