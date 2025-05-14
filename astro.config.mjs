629ef593a6a2042b2c491ee535e46afd541bc4b8
Martín Rojas <csamuelr.lp@gmail.com>
2025-05-14 20:55:17 -0300

    Configura base para deploy en subcarpeta de GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://retak1ng.github.io',
-	//base: 'my-repo',
-	//integrations: [mdx(), sitemap()],
+  site: 'https://retak1ng.github.io/martindev/',
+  base: '/martindev/',
+  //integrations: [mdx(), sitemap()],
 });
