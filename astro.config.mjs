bd1a563ca8b6b87a12398c1499c54a370a50c92b
Martín Rojas <csamuelr.lp@gmail.com>
2025-05-14 20:13:38 -0300

    deploy fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://retak1ng.github.io',
-	base: 'my-repo',
-	integrations: [mdx(), sitemap()],
+	//base: 'my-repo',
+	//integrations: [mdx(), sitemap()],
 });
