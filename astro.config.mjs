08ef9d96cd2bccc83a9dfc89f2a0ec1bef2c0c30
Martín Rojas <csamuelr.lp@gmail.com>
2025-05-14 21:08:31 -0300

    links fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://retak1ng.github.io/martindev/',
   base: '/martindev/',
-  //integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap()],
 });
